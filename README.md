0c7b5e5b10840318339663e9599eab49e7b3d62c
Josh Fung <js404839@dal.ca>
2020-12-12 20:21:43 -0500

    Can now store things on chrome

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 Wishlist cart that contains items you can wishlist from multiple e-commerce websites all stored in one place
 
 
+The wishlist is stored on the chrome broswer with chrome's localStorage API
+
 Currently focusing on getting functionality in the following order
     1. Newegg
     2. Amazon
--- background.js
+++ background.js
@@ -1,1 +1,20 @@
 console.log("Wishlist-Cart-Extension is running");
+
+//Objects that will store the items for their respective store
+//The way we store objects is tentative to change
+let neweggJSON = {
+    table: []
+};
+let amazonJSON = {
+    table: []
+};
+
+//When the extension is first installed, store these empty objects so we can add items to them later
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({newegg: neweggJSON}, function() {
+      console.log("Newegg JSON object set to " + neweggJSON);
+    });
+    chrome.storage.sync.set({amazon: amazonJSON}, function() {
+        console.log("Amazon JSON object set to " + amazonJSON);
+      });
+});
